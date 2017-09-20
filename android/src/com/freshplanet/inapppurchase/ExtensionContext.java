178727cc7c2096a7acbbdd27cfaa2346de9c57bb
Mustang Kong <mustang2247@users.noreply.github.com>
2017-09-20 11:32:21 +0800

    Revert "android: gradle, updated billing AIDL, restructured"
    This reverts commit d6ff8f3c7f7d587f628626dbb13ff43a698c9c68.

--- android/src/com/freshplanet/inapppurchase/ExtensionContext.java
+++ android/src/com/freshplanet/inapppurchase/ExtensionContext.java
@@ -1,18 +1,22 @@
-/**
- * Copyright 2017 FreshPlanet
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.freshplanet.ane.AirInAppPurchase;
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 2012 Freshplanet (http://freshplanet.com | opensource@freshplanet.com)
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  
+//////////////////////////////////////////////////////////////////////////////////////
+
+package com.freshplanet.inapppurchase;
 
 import java.util.HashMap;
 import java.util.List;
@@ -346,15 +350,12 @@ public class ExtensionContext extends FREContext {
         }
     };
 
-    // --------------------------------------------------------------------------------------//
-    //																						 //
-    // 									 	FREContext SETUP								 //
-    // 																						 //
-    // --------------------------------------------------------------------------------------//
-
     /**
      *
+     * FREContext SETUP
+     *
      */
+
     public void dispose() {
 
         _freActivity = null;
@@ -378,10 +379,6 @@ public class ExtensionContext extends FREContext {
         }
     }
 
-    /**
-     *
-     * @return
-     */
     public Map<String, FREFunction> getFunctions() {
 
         Map<String, FREFunction> functionMap = new HashMap<String, FREFunction>();
